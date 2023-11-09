ce4b15ae8304b2277f576ba69bfc0f78bdb4ea9d
Andreas Frølund Mogensen <cph-am526@cphbusiness.dk>
2023-11-09 15:13:32 +0100

    update biler

--- githubdemo/src/Main.java
+++ githubdemo/src/Main.java
@@ -1,4 +1,56 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+@startuml Matador
+
+        !define BORDERCOLOR DarkSlateGray
+        !define BGCOLOR LightGray
+        !define ELEMENTCOLOR CornflowerBlue
+
+        skinparam class {
+        BorderColor $BORDERCOLOR
+        BackgroundColor $BGCOLOR
+        }
+
+        skinparam usecase {
+        BackgroundColor $BGCOLOR
+        }
+
+        package "Matador Spillebræt" {
+class "Start" as start {
+        +På start
+        }
+
+class "Felt" as felt {
+        +Ejendom
+        +Pris
+        }
+
+class "Gå i fængsel" as faengsel {
+        +Gå i fængsel
+        }
+
+class "Gratis parkering" as parkering {
+        +Gratis parkering
+        }
+
+class "Chancekort" as chance {
+        +Træk chancekort
+        }
+
+class "Skat" as skat {
+        +Betal skat
+        }
+
+class "Ejendomsgruppe" as gruppe {
+        +Pris for hus
+        +Pris for hotel
+        }
+        }
+
+        start --> felt
+        felt --> felt
+        felt --> faengsel
+        felt --> parkering
+        felt --> chance
+        felt --> skat
+        felt --> gruppe
+
+@enduml
